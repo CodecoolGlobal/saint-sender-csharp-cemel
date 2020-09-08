445386e1d894b4607b300ddd493fb03074ada4c2
Lilla Kertesz <lil.kertesz@gmail.com>
2020-09-08 19:33:31 +0200

    get all messages in raw Base64 format

--- SaintSender.Core/Services/DataHandler.cs
+++ SaintSender.Core/Services/DataHandler.cs
@@ -2,6 +2,7 @@
 using Google.Apis.Gmail.v1.Data;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,16 +22,28 @@ namespace SaintSender.Core.Services
             //get our emails   
 
             IList<Message> messages = request1.Execute().Messages;
-            IList<String> snippets = new List<String>();
+            IList<String> rawMessages = new List<String>();
             foreach (var mail in messages)
             {
                 var mailId = mail.Id;
-                var threadId = mail.ThreadId;
-
-                Message message = service.Users.Messages.Get("me", mailId).Execute();
-                snippets.Add(message.Snippet);
+                var request = service.Users.Messages.Get("me", mailId);
+                request.Format = UsersResource.MessagesResource.GetRequest.FormatEnum.Raw;
+                var message = request.Execute();
+                //Debug.Write(DecodeBase64String(message.Raw));
+                //rawMessages.Add(DecodeBase64String(message.Raw));
+                Debug.Write(message.Raw);
+                rawMessages.Add(message.Raw);
             }
-            return snippets;
+            return rawMessages;
         }
+        //static String DecodeBase64String(string s)
+        //{
+        //    var ts = s.Replace("-", "+");
+        //    ts = ts.Replace("_", "/");
+        //    var bc = Convert.FromBase64String(ts);
+        //    var tts = Encoding.UTF8.GetString(bc);
+
+        //    return tts;
+        //}
     }
 }
