4a43c6094e7a86b7cd4040f45ce32a560d2de482
Balint Molnar <balintkilencvenegy@gmail.com>
2020-09-09 11:30:06 +0200

    Add GetMessageBodies() + refactoring

--- SaintSender.Core/Entities/Email.cs
+++ SaintSender.Core/Entities/Email.cs
@@ -8,22 +8,7 @@ namespace SaintSender.Core.Entities
 {
     public class Email
     {
-        public Email(string sender, string date, string subject, string body)
-        {
-            Sender = sender;
-            Date = date;
-            Subject = subject;
-            Body = body;
-            //ID = id;
-        }
-
-
-
-        public string Sender
-        {
-            get;
-            set;
-        }
+        public string Sender { get; set; }
 
         public string Date { get; set; }
 
@@ -31,6 +16,12 @@ namespace SaintSender.Core.Entities
 
         public string Body { get; set; }
 
-        public int ID { get; set; }
+        public Email(string sender, string date, string subject, string body)
+        {
+            Sender = sender;
+            Date = date;
+            Subject = subject;
+            Body = body;
+        }
     }
 }
--- SaintSender.DesktopUI/ViewModels/MainViewModel.cs
+++ SaintSender.DesktopUI/ViewModels/MainViewModel.cs
@@ -4,43 +4,71 @@ using SaintSender.Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-
+using System.Diagnostics;
+using System.Text;
 
 namespace SaintSender.DesktopUI.ViewModels
 {
     class MainViewModel
     {
-        private List<Message> receivedEMails;
-        private ObservableCollection<Email> emailToSHow = new ObservableCollection<Email>();
+        private List<Message> receivedEmails;
+        public ObservableCollection<Email> emailToShow = new ObservableCollection<Email>();
 
-        public List<Message> getEmails()
+        public List<Message> GetEmails()
         {
-            List<Message> finalMessages = new List<Message>();
             Pop3Client client = new Pop3Client();
             client.Connect("pop.gmail.com", 995, true);
             client.Authenticate("csharptw5@gmail.com", "Csharp123", AuthenticationMethod.UsernameAndPassword);
-            int messageCount = client.GetMessageCount();
-            
 
-            for(int i = 1;i < messageCount; i++)
+            List<Message> messages = new List<Message>();
+            int messageCount = client.GetMessageCount();
+
+            for (int i = 1; i < messageCount; i++)
             {
-                finalMessages.Add(client.GetMessage(i));
+                messages.Add(client.GetMessage(i));
             }
-             return finalMessages;   
+
+            return messages;
         }
 
-        public void setupEmails()
+        public void SetupEmails()
         {
-            receivedEMails = getEmails();
+            receivedEmails = GetEmails();
+        }
+
+        public List<string> GetMessageBodies()
+        {
+            var messageBodies = new List<string>();
+
+            foreach (Message message in receivedEmails)
+            {
+                MessagePart plainText = message.FindFirstPlainTextVersion();
+
+                if (plainText != null)
+                {
+                    messageBodies.Add(plainText.GetBodyAsText());
+                }
+                else
+                {
+                    MessagePart html = message.FindFirstHtmlVersion();
+
+                    if (html != null)
+                    {
+                        messageBodies.Add(html.GetBodyAsText());
+                    }
+                }
+            }
+
+            return messageBodies;
         }
 
         public void BuildUpEmailsToShow()
         {
-
-            foreach(Message message in receivedEMails)
+            foreach (Message message in receivedEmails)
             {
-                string body;
                 MessagePart messagePart = message.FindFirstPlainTextVersion();
+                string body;
+
                 if (!message.MessagePart.IsMultiPart)
                 {
                     body = message.MessagePart.GetBodyAsText();
@@ -49,17 +77,16 @@ namespace SaintSender.DesktopUI.ViewModels
                 {
                     body = messagePart.GetBodyAsText();
                 }
-                
 
-
-
-                emailToSHow.Add(new Email(message.Headers.ReturnPath.Address, message.Headers.Date, message.Headers.Subject, body));
-
+                try
+                {
+                    emailToShow.Add(new Email(message.Headers.ReturnPath.Address, message.Headers.Date, message.Headers.Subject, body));
+                }
+                catch (NullReferenceException e)
+                {
+                    Debug.WriteLine(e.Message);
+                }
             }
-
         }
-
-
-        
     }
 }
