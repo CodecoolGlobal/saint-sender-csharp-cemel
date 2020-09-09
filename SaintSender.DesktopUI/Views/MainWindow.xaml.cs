23bd84dcca741647a3f3f8a14a923fa06de45642
Balint Molnar <balintkilencvenegy@gmail.com>
2020-09-09 19:55:05 +0200

    Add dynamic search and refresh timer

--- SaintSender.DesktopUI/Views/MainWindow.xaml.cs
+++ SaintSender.DesktopUI/Views/MainWindow.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -15,6 +17,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 using OpenPop.Mime;
 using SaintSender.Core.Entities;
 using SaintSender.Core.Services;
@@ -30,19 +33,41 @@ namespace SaintSender.DesktopUI
     {
         private MainViewModel _vm = new MainViewModel();
         private ObservableCollection<Email> emailToSHow;
+        private List<Email> allEmails;
+        private DispatcherTimer timer = new DispatcherTimer();
 
         public MainWindow()
         {
             InitializeComponent();
             _vm.SetupEmails();
             emailToSHow = _vm.BuildUpEmailsToShow();
+            allEmails = emailToSHow.ToList<Email>();
             DataContext = this;
             //SaintSender.Core.Services.Sender.SendEmail("lilaalex95@gmail.com", "okeka", "neeeeeecsinald");
+
+            timer.Interval = new TimeSpan(0, 0, 5);
+            timer.Tick += Timer_Tick;
+            timer.Start();
         }
 
         public ObservableCollection<Email> EmailsToDisplay
         {
             get { return emailToSHow; }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (txtSearch.Text == null || txtSearch.Text == "")
+            {
+                RefreshEmailList();
+            }
+        }
+
+        private void RefreshEmailList()
+        {
+            _vm.SetupEmails();
+            emailToSHow = _vm.BuildUpEmailsToShow();
+            allEmails = emailToSHow.ToList<Email>();
         }
 
         private void writeMail_Click(object sender, RoutedEventArgs e)
@@ -53,16 +78,28 @@ namespace SaintSender.DesktopUI
 
         private void btnSearch_Click(object sender, RoutedEventArgs e)
         {
-            string text = txtSearch.Text;
-            SearchInEmail(text);
+            SearchInEmails(txtSearch.Text);
         }
 
-        private void SearchInEmail(string text)
+        private void txtSearch_TextChanged(object sender, TextChangedEventArgs e)
         {
-            string pattern = $".*{text}.*";
+            SearchInEmails(txtSearch.Text);
+        }
 
-            var query = from item in emailToSHow
-                        where Regex.IsMatch(item.Body, pattern)
+        private void SearchInEmails(string text)
+        {
+            string pattern = ".*";
+
+            if (text != null && text != "")
+            {
+                pattern = $".*{text.ToUpperInvariant()}.*";
+            }
+
+            var query = from item in allEmails
+                        where (Regex.IsMatch(item.Body.ToUpperInvariant(), pattern)
+                            || Regex.IsMatch(item.Date.ToUpperInvariant(), pattern)
+                            || Regex.IsMatch(item.Sender.ToUpperInvariant(), pattern)
+                            || Regex.IsMatch(item.Subject.ToUpperInvariant(), pattern))
                         select item;
 
             ObservableCollection<Email> bob = new ObservableCollection<Email>(query);
